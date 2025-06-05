af0fb936e19313d6a73fa126c0f7daf7aab157eb
usmanz0 <usmanrizwanshamshi125@gmail.com>
2025-06-05 21:14:50 +0500

    testing javafx

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,40 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+	 
+	 public static void main(String[] args) {
+	  launch(args);
+	 }
+
+	 @Override
+	 public void start(Stage primaryStage) throws Exception {
+	  Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+
+	  Scene scene = new Scene(root,1080,720,Color.rgb(18, 18, 18, 1));
+	  Stage stage = new Stage();
+	  Image icon = new Image("nexlaunchlogo.png");
+	  stage.getIcons().add(icon);
+	  stage.setTitle("NexLaunch");
+	  
+	  stage.setScene(scene);
+	  stage.show();
+	 }
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module NexLaunch {
 	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
