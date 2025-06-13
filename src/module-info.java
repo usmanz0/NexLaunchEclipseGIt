0976737cac0cb636ed285dd9907e9803ee4a41a1
Syed Hussain <hussainuddin119@gmail.com>
2025-06-13 23:21:17 +0500

    Store Url and shortcut

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
 module NexLaunch {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires java.desktop;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.base;
+    requires com.google.gson;
+
+    opens application to javafx.graphics, javafx.fxml, com.google.gson;
 }
