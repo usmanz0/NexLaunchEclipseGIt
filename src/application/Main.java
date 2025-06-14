edbb65476cd83dd946c5ef58155a73dd583d15ac
usmanz0 <usmanrizwanshamshi125@gmail.com>
2025-06-14 11:42:31 +0500

    idk

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,18 +3,11 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+
 
 
 public class Main extends Application {
@@ -31,7 +24,6 @@ public class Main extends Application {
 			  
 			  stage.getIcons().add(icon);
 			  stage.setTitle("NexLaunch");
-			  
 			  
 			  
 			  stage.setScene(scene);
--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,5 @@ module NexLaunch {
 	requires javafx.fxml;
 	requires java.desktop;
 	requires javafx.base;
-	
 	opens application to javafx.graphics, javafx.fxml;
 }
