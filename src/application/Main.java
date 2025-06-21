03c66511f61a399b888474db8e4d77eee5cad259
Syed Hussain <hussainuddin119@gmail.com>
2025-06-21 17:03:39 +0500

    last commit???

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader; // Import FXMLLoader
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,39 +10,30 @@ import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
-    private Controller controller; // Declare the controller field
+    private Controller controller; 
     
     @Override
-    public void start(Stage primaryStage) { // Use primaryStage for the main window
-        // Removed: All runInBackground checks and conditional logic
+    public void start(Stage primaryStage) { 
 
         System.out.println("Main: Application starting in normal GUI mode.");
         try {
-            // 1. Create an FXMLLoader instance
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
 
-            // 2. Load the FXML using the loader instance
             Parent root = loader.load();
 
-            // 3. Get the controller instance from the loader
             controller = loader.getController();
 
-            // Removed: controller.setBackgroundMode(false); // No longer needed
 
-            // Setup the Scene
             Scene scene = new Scene(root, 1080, 720, Color.rgb(18, 18, 18, 1));
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-            // Setup the Stage (primaryStage is already provided by JavaFX)
-            Image icon = new Image("nexlaunchlogo.png"); // Make sure nexlaunchlogo.png is in src/main/resources/
+            Image icon = new Image("nexlaunchlogo.png");
             primaryStage.getIcons().add(icon);
             primaryStage.setTitle("NexLaunch");
 
-            // Set the scene on the primaryStage
             primaryStage.setScene(scene);
             primaryStage.show();
 
-            // Set the close request handler on the primaryStage
             primaryStage.setOnCloseRequest(event -> {
                 if (controller != null) {
                     controller.saveLaunchers();
@@ -57,7 +48,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        // Pass command line arguments to the JavaFX application lifecycle
         launch(args);
     }
 }
